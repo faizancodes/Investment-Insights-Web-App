f8437aa182e15ac44027d6945b89eb59e04e206c
Faizan Ahmed <43652410+faizancodes@users.noreply.github.com>
2019-07-11 15:08:06 -0400

    Add files via upload

---
+++ static/js/script.js
@@ -0,0 +1,9 @@
+﻿$.getJSON('static/ratings.json', function (data) {
+    console.log(data);
+    var output = '<ul>';
+    $.each(data, function (key, val) {
+        output += '<li>' + val.symbol + " " + val.overallRating + '</li>';
+    });
+    output += '</ul>';
+    $('#update').html(output);
+});
